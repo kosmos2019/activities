e08da1b71553d4f4b81ee1f8ecc8653daa147277
BYAD, Jawad <jawad.byad@capgemini.com>
2021-05-20 19:23:20 +0100

    End of section 10

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
 using MediatR;
 using Persistence;
@@ -9,12 +10,12 @@ namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<Result<Activity>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, Result<Activity>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -22,9 +23,14 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.FindAsync(request.Id);
+                Activity activity = await _context.Activities.FindAsync(request.Id);
+
+                //if (activity == null) throw new Exception("Activity not found");
+
+                return (activity != null) ? Result<Activity>.Success(activity) :
+                    Result<Activity>.Failure("activity not found");
             }
         }
     }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
 import NavBar from './NavBar';
 import HomePage from '../../features/home/HomePage';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestErrors';
+import NotFound from '../../features/errors/NotFound';
+import { ToastContainer } from 'react-toastify';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route path='/' exact component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -20,9 +25,14 @@ function App() {
           <>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-              <Route path='/activities' exact component={ActivityDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Switch>
+                <Route path='/activities' exact component={ActivityDashboard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+                <Route path='/errors' component={TestErrors} />
+                <Route path='/server-error' component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
