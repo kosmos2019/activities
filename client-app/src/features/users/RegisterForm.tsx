6c80ab59579b98fe5c13f81c3ea6cb89ac805959
BYAD, Jawad <jawad.byad@capgemini.com>
2021-06-08 11:50:43 +0100

    End section 13

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Formik, Form, ErrorMessage } from 'formik';
 import MyTextInput from '../../app/common/form/MyTextInput';
-import { Button, Header, Label } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
 import * as Yup from 'yup';
 import ValidationErrors from '../errors/ValidationErrors';
